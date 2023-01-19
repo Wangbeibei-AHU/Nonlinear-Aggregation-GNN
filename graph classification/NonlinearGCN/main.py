a94b18c5b3e0b0de711f83d6ea9bc40e2731f375
Wangbeibei-AHU <91249709+Wangbeibei-AHU@users.noreply.github.com>
2023-01-19 10:40:49 +0800

    Update main.py

--- graph classification/NonlinearGCN/main.py
+++ graph classification/NonlinearGCN/main.py
@@ -1,17 +1,27 @@
 from experiments import experiment
 import numpy as np
+import argparse
 import scipy.io as sio
 import pdb
 
 
-# ## single dataset
-# epochs = 500
-# kfold_num = 1
-# test_acc = np.zeros(kfold_num)
-# test_accs = np.zeros([kfold_num, epochs])
-# dataset = "mutag" # mutag, imdb-b, imdb-m, ptc-mr, reddit-b, proteins
-# for kfold in range(kfold_num):
-# 	test_acc[kfold], test_accs[kfold] = experiment(dataset=dataset,model="nlgingcn", epochs=epochs, dropout=0.3, batch_size=128, lr=0.01, kfold=kfold, test_ratio=0.1, use_best_config=True, mw="graph_classification_mw", dw="graph_classification_dw")
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='mutag', help='dataset')
+parser.add_argument('--epochs', type=int,  default=500, help='Number of epochs to train.')
+parser.add_argument("--batch_size", type=int, default=32)
+parser.add_argument("--hidden_size", type=int, default=64)
+parser.add_argument("--num_layers", type=int, default=3)
+parser.add_argument("--kfold_num", type=int, default=10)
+parser.add_argument("--lr", type=float, default=0.001)
+parser.add_argument("--dropout", type=float, default=0.3)
+parser.add_argument("--mod", type=str, default="Softmax", choices=['Generalized-mean', 'Polynomial', 'Softmax'])
+args = parser.parse_args()
+
+# test_acc = np.zeros(args.kfold_num)
+# test_accs = np.zeros([args.kfold_num, args.epochs])
+# for kfold in range(args.kfold_num):
+# 	test_acc[kfold], test_accs[kfold] = experiment(dataset=args.dataset, model="NonlinearGCN", mod=args.mod, num_layers=args.num_layers, batch_size=args.batch_size, lr=args.lr, dropout=args.dropout, hidden_size=args.hidden_size, epochs=args.epochs, kfold=kfold, test_ratio=0.1, use_best_config=True, mw="graph_classification_mw", dw="graph_classification_dw")
 # mean_test_acc = np.mean(test_accs, axis=0)
 # best_epoch = np.argmax(mean_test_acc)
 # mean_acc = np.mean(test_accs[:, best_epoch])
@@ -19,19 +29,14 @@ import pdb
 # print('test_acc: ', mean_acc, ' best_std: ', mean_std)
 
 
-
-##multi-datasets
-epochs = 500
-kfold_num = 10
-batch_size = 32
-num_layer = 3
-
+## Tips
+"Note that If the results are inconsistent with reported ones due to differences in python environment or equipment," 
+"you can get your own parameters in the following ways:"
 datasets = ['ptc-mr', 'proteins','mutag']#, 'ptc-mr', 'proteins'
 dnum = len(datasets)
 lrs = [0.01, 0.001]
 dropouts = [0., 0.3, 0.5]
 hidden_size = [32, 64]
-
 
 best_lr = np.zeros(dnum)
 best_dropout=np.zeros(dnum)
@@ -44,18 +49,18 @@ for i in range(dnum):
 	for h in hidden_size: 
 		for lr in lrs:
 			for d in dropouts:
-				test_acc = np.zeros(kfold_num)
-				test_accs = np.zeros([kfold_num, epochs])
+				test_acc = np.zeros(args.kfold_num)
+				test_accs = np.zeros([args.kfold_num, args.epochs])
 				results_acc = []
 				results_std = []
-				for kfold in range(kfold_num):
-					test_acc[kfold], test_accs[kfold] = experiment(dataset=dataset, model="NonlinearGCN", mod='Generalized-mean', num_layers=num_layer, batch_size=batch_size, lr=lr, dropout=d, hidden_size=h, epochs=epochs, kfold=kfold, test_ratio=0.1, use_best_config=True, mw="graph_classification_mw", dw="graph_classification_dw")
+				for kfold in range(args.kfold_num):
+					test_acc[kfold], test_accs[kfold] = experiment(dataset=dataset, model="NonlinearGCN", mod=args.mod, num_layers=args.num_layers, batch_size=args.batch_size, lr=lr, dropout=d, hidden_size=h, epochs=args.epochs, kfold=kfold, test_ratio=0.1, use_best_config=True, mw="graph_classification_mw", dw="graph_classification_dw")
 				
 				mean_test_acc = np.mean(test_accs, axis=0)
 				best_epoch = np.argmax(mean_test_acc)
 				mean_acc = np.mean(test_accs[:, best_epoch])
 				mean_std = np.std(test_accs[:, best_epoch])
-				print('dataset: ',dataset, ' lr=', lr, ' batch_size=', b, ' hidden_size=', h, ' dropout', d, ' test_acc: ', mean_acc, ' best_std: ', mean_std)
+				print('dataset: ',dataset, ' lr=', lr, ' hidden_size=', h, ' dropout', d, ' test_acc: ', mean_acc, ' best_std: ', mean_std)
 				results_acc.append(mean_acc) 
 				results_std.append(mean_std)
 				if mean_acc > best_test_acc[i]:
